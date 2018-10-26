122a9122d8007bbc1ca4537de2927a56ee14b6a2
Macianer <the_macianer@gmx.de>
2018-10-26 17:46:55 +0200

    convert upstream to python 2

--- WPBackup.py
+++ WPBackup.py
@@ -111,27 +111,28 @@ def main(argv):
     wp_dir = u''
     backup_directory = u''
     try:
-        opts, _ = getopt.getopt(argv, 'w:b', ['wordpress_directory=', 'backup_directory='])
+        opts, _ = getopt.getopt(argv, u'hw:b', [u'wordpress_directory=', u'backup_directory='])
     except getopt.GetoptError as err:
         print(str(err)) 
         usage()
         sys.exit(2)
     for opt, arg in opts:
-        if opt == '-h':
-            print('WPBackup.py -w <wordpress_directory> -b <backup_directory>')
+        print arg
+        if opt == u'-h':
+            print u'WPBackup.py -w <wordpress_directory> -b <backup_directory>'
             usage()
             sys.exit()
-        elif opt in ("-w", "--wordpress_directory"):
+        elif opt in (u"-w", u"--wordpress_directory"):
+            print "found wp"
             wp_dir = arg
         elif opt in (u"-b", u"--backup_directory"):
+            print "found b" + arg
             backup_directory = arg
     if len(wp_dir) < 1:
-        print("wordpress directory is missing")
-        usage()
+        print u"wordpress directory is missing"
         sys.exit()
     if len(backup_directory) < 1:
-        print("backup directory is missing")
-        usage()
+        print u"backup directory is missing"
         sys.exit()
 
     if os.path.exists(wp_dir):
@@ -144,9 +145,9 @@ def main(argv):
         print ("Finished backup with {0}").format(archive_path)
 
 def usage():
-    print("\n\nDescription:\n\nA script to backup wordpress website onto local folder.")
-    print("This backup will include file and database information related to Wordpress folder.\n")
-    print('\nUSAGE: ./WPBackup.py -w <wordpress_directory> -b <backup_directory>')
+    print u"\n\nDescription:\n\nA script to backup wordpress website onto local folder."
+    print u"This backup will include file and database information related to Wordpress folder.\n"
+    print u'\nUSAGE: ./WPBackup.py -w <wordpress_directory> -b <backup_directory>'
 
 if __name__ == u'__main__':
     main(sys.argv[1:])
